5ddabd4648eab1a6cf7fc8ab4b40997620a4d8b3
NuckenMcFuggets <cameron.ian.ronald.ord@Outlook.com>
2023-04-15 17:00:37 -0600

    created objects

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <header><page-header></page-header></header>
+    <main><page-main></page-main></main>
+    <footer><page-footer></page-footer></footer>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import PageHeader from './components/PageHeader.vue';
+import PageMain from './components/PageMain.vue';
+import PageFooter from './components/PageFooter.vue';
 export default {
-  name: 'App',
+  name: 'page_comps',
   components: {
-    HelloWorld
+ 
+    PageHeader,
+    PageMain,
+    PageFooter
+
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/DisplayPosts.vue
@@ -0,0 +1,55 @@
+<template>
+    <div>
+
+
+
+
+    </div>
+</template>
+
+<script>
+
+
+    export default {
+        data() {
+            return [
+                {
+
+                    content: ``,
+
+                    user: ``,
+
+                    date_created: ``
+
+
+                },
+
+                {
+
+                    content: ``,
+
+                    user: ``,
+
+                    date_created: ``
+
+
+                },
+
+                                {
+
+                    content: ``,
+
+                    user: ``,
+
+                    date_created: ``
+
+
+                },
+            ]
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
